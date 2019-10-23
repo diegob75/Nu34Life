4a9cc1784c11bf46697c6d3ca5037b07f99f9cf2
Diego <dredson71@gmail.com>
2019-10-23 10:37:46 -0500

    Service-recipe modified

--- backend/service-recipes/src/main/java/com/nutritech/nu34life/service/PKIngredientService.java
+++
@@ -1,7 +0,0 @@
-package com.nutritech.nu34life.service;
-
-import com.nutritech.nu34life.model.entity.PkIngredient;
-
-public interface PKIngredientService extends CrudService<PkIngredient, Long>{
-
-}
