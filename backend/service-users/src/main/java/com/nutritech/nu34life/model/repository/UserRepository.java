4c7bd9c3b2cb856656c1b27a3c09e825c5e8b403
unknown <adsg999@gmail.com>
2019-11-05 21:31:42 -0500

    service-users updated

---
+++ backend/service-users/src/main/java/com/nutritech/nu34life/model/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.nutritech.nu34life.model.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import com.nutritech.nu34life.model.entity.Account;
+
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<Account,Long>{
+
+	@Query("SELECT u FROM Account u WHERE u.username=?1")
+	Account getByUsername(String _username);
+
+    @RestResource(path="getUsername")
+	Account findByUsername(@Param("_username") String username);
+	
+}
