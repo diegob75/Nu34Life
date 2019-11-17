e445ac160a35277a77740301d44cb2036fe47ef1
marco <jitrix15@hotmail.com>
2019-11-17 17:23:31 -0500

    fixed

--- backend/service-profile/src/main/java/com/nutritech/nu34life/service/NutritionistService.java
+++
@@ -1,7 +0,0 @@
-package com.nutritech.nu34life.service;
-
-import com.nutritech.nu34life.entity.Nutritionist;
-
-public interface NutritionistService extends CrudService<Nutritionist,Long>{
-
-}
--- backend/service-profile/src/main/java/com/nutritech/nu34life/service/PatientService.java
+++
@@ -1,7 +0,0 @@
-package com.nutritech.nu34life.service;
-
-import com.nutritech.nu34life.entity.Patient;
-
-public interface PatientService extends CrudService<Patient, Long>{
-
-}
