4d4c86b7a55b73792b218262147d476bddc36f92
Fernando <fernandochahua1@gmail.com>
2019-11-17 22:36:12 -0500

    hot fix

--- backend/service-gateway/src/test/java/com/nutritech/nu34life/ServiceGatewayApplicationTests.java
+++ backend/service-gateway/src/test/java/com/nutritech/nu34life/ServiceGatewayApplicationTests.java
@@ -1,12 +1,11 @@
 package com.nutritech.nu34life;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServiceGatewayApplicationTests {
 
-	@Test
 	void contextLoads() {
 	}
 
