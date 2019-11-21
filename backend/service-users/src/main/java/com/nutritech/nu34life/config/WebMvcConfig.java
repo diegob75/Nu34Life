d9cad3716a8307f97ae9b3b7fd8e41e3b088ecbe
Fernando Chahua <fernandochahua1@gmail.com>
2019-11-21 13:06:18 -0500

    adding email service

--- backend/service-users/src/main/java/com/nutritech/nu34life/config/WebMvcConfig.java
+++ backend/service-users/src/main/java/com/nutritech/nu34life/config/WebMvcConfig.java
@@ -2,13 +2,10 @@ package com.nutritech.nu34life.config;
 
 
 
-import java.util.Properties;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -26,22 +23,5 @@ public class WebMvcConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-    }
-    @Bean
-    public JavaMailSender getJavaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-         
-        mailSender.setUsername("nutritech.no.reply@gmail.com");
-        mailSender.setPassword("nu34life#");
-         
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
-         
-        return mailSender;
     }
 }
